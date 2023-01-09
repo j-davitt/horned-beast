9fa5d760f6f4bcde0f4ec2b8eda5744bde42dd27
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-09 21:45:33 +0000

    add HornedBeasts to Main

--- src/Main.js
+++ src/Main.js
@@ -6,10 +6,9 @@ class Main extends React.Component{
     return (
       <>
         <h3>Hello There!</h3>
-        <article>
-          <HornedBeast />
-          <HornedBeast />
-        </article>
+        <HornedBeast title="Uniwhal" imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="narwhal" description="A unicorn and a narwhal"/>
+        <HornedBeast title="Rhino Family" imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" alt="rhino" description="Parent rhino with babies"/>
+        
       </>
     )
   }
