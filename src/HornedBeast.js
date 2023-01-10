2320cbb20dae483fb522dbac1c87f99aa975fb74
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-10 19:30:10 +0000

    add bootstrap

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 class HornedBeast extends React.Component{
   constructor(props){
@@ -19,10 +20,10 @@ class HornedBeast extends React.Component{
       <>
         <article>
           <h2>{this.props.title}</h2>
+          <p>❤️{this.state.votes} favorites.</p>
           <img src={this.props.image_url} alt={this.props.keyword}></img>
           <p>{this.props.description}</p>
-          <button onClick={this.handleVote}>Vote Here!</button>
-          <p>{this.state.votes} votes.</p>
+          <Button onClick={this.handleVote} variant="danger">Favorite</Button>
         </article>
       </>
     )
