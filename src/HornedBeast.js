24468e67d32950f1b0448eb06fb741c1397ea2b5
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-10 19:13:05 +0000

    add vote functionality

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+
+  handleVote = () => {
+    this.setState({
+      votes: this.state.votes + 1
+    })
+  }
+
   render(){
     return (
       <>
@@ -8,6 +21,8 @@ class HornedBeast extends React.Component{
           <h2>{this.props.title}</h2>
           <img src={this.props.image_url} alt={this.props.keyword}></img>
           <p>{this.props.description}</p>
+          <button onClick={this.handleVote}>Vote Here!</button>
+          <p>{this.state.votes} votes.</p>
         </article>
       </>
     )
